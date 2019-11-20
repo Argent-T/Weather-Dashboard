4e50a2d87bdc97167b27dc577c4971acd38e0a61
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2019-11-20 12:42:39 -0500

    progress

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,27 @@
 
 
 var city = "New York"
-var lat = 0;
-var lon = 0;
-var bob = 0;
 
 
-getCurrentCityWeather()
+
+
+
 
 function getCurrentCityWeather() {
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=7a7a18a0877364062aba3b94fdafdc1d"
-    var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=7a7a18a0877364062aba3b94fdafdc1d&lat=" + lat + "&lon=" + lon + "&cnt=5"
-    // var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=7a7a18a0877364062aba3b94fdafdc1d&lat=40&lon=-73&cnt=5"
-
-
-    bob=2;
+//    Weather data//////////////////////////////////////////////////////////////////
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-
         var temperature = [];
         var humidity = [];
         var windSpeed = [];
         var icon = [];
         var description = [];
-
-
+        var lat = 0;
+        var lon = 0;
         for (i = 0; i < 10; i++) {
             temperature.push(((response.list[i].main.temp - 273.15) * (9 / 5) + 32).toFixed(0))
             humidity.push(response.list[i].main.humidity);
@@ -35,7 +29,6 @@ function getCurrentCityWeather() {
             icon.push(response.list[i].weather[0].icon);
             description.push(response.list[i].weather[0].description);
         }
-        bob = 1;
         lat = response.city.coord.lat;
         lon = response.city.coord.lon;
         console.log("wind " + windSpeed);
@@ -43,24 +36,32 @@ function getCurrentCityWeather() {
         console.log("temperature " + temperature);
         console.log("icon " + icon);
         console.log("description " + description);
+        console.log("lat " + lat)
+        console.log("lon " + lon)
+
+// UV data//////////////////////////////////////////////////////////////////
+        var queryUV = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=7a7a18a0877364062aba3b94fdafdc1d&lat=" + lat + "&lon=" + lon + "&cnt=5"
+        $.ajax({
+            url: queryUV,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+    
+    
+        });
+    
+
 
     })
 
-    $.ajax({
-        url: queryUV,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+   
 
 
-    });
-
-
-
-    console.log("lat " + lat)
-    console.log("lon " + lon)
-    console.log("bob " + bob)
+   
+   
 
 
 
 }
+
+getCurrentCityWeather()
