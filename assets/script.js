1d502852c911c783c4d84539bc109ad51695c97b
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2019-11-20 11:43:11 -0500

    api

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ function getCurrentCityWeather() {
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=7a7a18a0877364062aba3b94fdafdc1d"
     var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=7a7a18a0877364062aba3b94fdafdc1d&lat=" + lat + "&lon=" + lon + "&cnt=5"
     // var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=7a7a18a0877364062aba3b94fdafdc1d&lat=40&lon=-73&cnt=5"
-    bob = 1
+  
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -33,7 +33,7 @@ function getCurrentCityWeather() {
             icon.push(response.list[i].weather[0].icon);
             description.push(response.list[i].weather[0].description);
         }
-        
+        bob = 1;
         lat = response.city.coord.lat;
         lon = response.city.coord.lon;
         console.log("wind " + windSpeed);
