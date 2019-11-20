a0c5ab49ca21727fa9a423df65dd693b6d88e262
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2019-11-20 11:34:35 -0500

    api1

--- assets/script.js
+++ assets/script.js
@@ -3,17 +3,17 @@
 var city = "New York"
 
 
-
+var lat = 0;
+var lon = 0;
 
 getCurrentCityWeather()
 
 function getCurrentCityWeather() {
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=7a7a18a0877364062aba3b94fdafdc1d"
-    var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=7a7a18a0877364062aba3b94fdafdc1d&lat=" + lat + "&lon=" + lon + "cnt=5"
+    var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=7a7a18a0877364062aba3b94fdafdc1d&lat=" + lat + "&lon=" + lon + "&cnt=5"
+    // var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=7a7a18a0877364062aba3b94fdafdc1d&lat=40&lon=-73&cnt=5"
+  
 
-    var lat = 0;
-    var lon = 0;
-   
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -34,7 +34,7 @@ function getCurrentCityWeather() {
             icon.push(response.list[i].weather[0].icon);
             description.push(response.list[i].weather[0].description);
         }
-        bob=1;
+        bob = 1;
         lat = response.city.coord.lat;
         lon = response.city.coord.lon;
         console.log("wind " + windSpeed);
@@ -44,21 +44,18 @@ function getCurrentCityWeather() {
         console.log("description " + description);
         console.log("lat " + lat)
         console.log("lon " + lon)
-        
+    });
+
+    $.ajax({
+        url: queryUV,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
 
 
-    }).then(
-
-        $.ajax({
-            url: queryUV,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
+    });
 
 
-        })
-
-    );
 
 
 
