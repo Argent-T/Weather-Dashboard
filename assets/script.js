834198256ac8177529ea87044df61864361196e2
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2019-11-20 12:13:09 -0500

    testing

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,32 @@
 
 
 var city = "New York"
-
-
 var lat = 0;
 var lon = 0;
 var bob = 0;
+
+
 getCurrentCityWeather()
 
 function getCurrentCityWeather() {
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=7a7a18a0877364062aba3b94fdafdc1d"
     var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=7a7a18a0877364062aba3b94fdafdc1d&lat=" + lat + "&lon=" + lon + "&cnt=5"
     // var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=7a7a18a0877364062aba3b94fdafdc1d&lat=40&lon=-73&cnt=5"
-  
+
+
+    bob=2;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        
+
         var temperature = [];
         var humidity = [];
         var windSpeed = [];
         var icon = [];
         var description = [];
-      
+
 
         for (i = 0; i < 10; i++) {
             temperature.push(((response.list[i].main.temp - 273.15) * (9 / 5) + 32).toFixed(0))
@@ -41,8 +43,8 @@ function getCurrentCityWeather() {
         console.log("temperature " + temperature);
         console.log("icon " + icon);
         console.log("description " + description);
-        
-    });
+
+    })
 
     $.ajax({
         url: queryUV,
